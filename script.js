4a559b76751c1307d5bf6daabc5c0154f457fbfe
Gabriella Mondvay <gabriellamondvay@gmail.com>
2023-12-13 10:14:41 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Sets starting values for round number and score.
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Generates a random number between 0 and 9, rounding down to nearest integer.
+const generateTarget = () => Math.floor(Math.random() * 10);
+
+// Defines an outer function for comparing which guess is closest to the target number. Returns true if humanDifference is smaller than or equal to computerDifference. Returns false otherwise. Inner function returns the absolute value between the human/computer guess and the target number.
+const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+  const getAbsoluteDistance = (number1, number2) => Math.abs(number1 - number2);
+  const humanDifference = getAbsoluteDistance(secretTarget - humanGuess);
+  const computerDifference = getAbsoluteDistance(secretTarget - computerGuess);
+  return humanDifference <= computerDifference;
+};
+
+// Determines the winner and returns a string indicating the winner.
+const updateScore = (winner) => {
+  if (winner === "human") {
+    return "human";
+  } else if (winner === "computer") {
+    return "computer";
+  }
+};
+
+// Adds 1 to the round every time a round is completed.
+function advanceRound() {
+  currentRoundNumber += 1;
+}
+
+// Alerts the user when their guess is out of range
