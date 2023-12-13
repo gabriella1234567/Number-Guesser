4043b3b2c223024bcafb66124e129c0722319e39
Gabriella Mondvay <gabriellamondvay@gmail.com>
2023-12-13 10:19:28 +0100

    Added

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const generateTarget = () => Math.floor(Math.random() * 10);
 // Defines an outer function for comparing which guess is closest to the target number. Returns true if humanDifference is smaller than or equal to computerDifference. Returns false otherwise. Inner function returns the absolute value between the human/computer guess and the target number.
 const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
   const getAbsoluteDistance = (number1, number2) => Math.abs(number1 - number2);
+  // Alerts the user when their guess is out of range
+  if (humanGuess < 0 || humanGuess > 9) {
+    alert("Please enter a number between 0 and 9");
+    return false;
+  }
   const humanDifference = getAbsoluteDistance(secretTarget - humanGuess);
   const computerDifference = getAbsoluteDistance(secretTarget - computerGuess);
   return humanDifference <= computerDifference;
@@ -27,5 +32,3 @@ const updateScore = (winner) => {
 function advanceRound() {
   currentRoundNumber += 1;
 }
-
-// Alerts the user when their guess is out of range
